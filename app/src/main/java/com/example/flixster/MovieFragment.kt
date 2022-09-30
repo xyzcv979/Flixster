f274ae73af58156073106972e9c2ea6a741c8c4e
Alex Ng <avon1462@gmail.com>
2022-09-30 19:17:41 -0400

    Connected to api request and finished app

--- app/src/main/java/com/example/flixster/MovieFragment.kt
+++ app/src/main/java/com/example/flixster/MovieFragment.kt
@@ -5,10 +5,16 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.widget.ContentLoadingProgressBar
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.RequestParams
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import okhttp3.Headers
+import org.json.JSONArray
 import org.json.JSONObject
 
 private const val API_KEY = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
@@ -26,7 +32,10 @@ class MovieFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_movie_view, container, false)
-
+        val recyclerView = view.findViewById<View>(R.id.list) as RecyclerView
+        val context = view.context
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        updateAdapter(recyclerView)
         return view
     }
 
@@ -34,7 +43,38 @@ class MovieFragment : Fragment() {
      * Updates the RecyclerView adapter with new data.  This is where the
      * networking magic happens!
      */
-    private fun updateAdapter(progressBar: ContentLoadingProgressBar, recyclerView: RecyclerView) {
+    private fun updateAdapter(recyclerView: RecyclerView) {
+        val client = AsyncHttpClient()
+        val params = RequestParams()
+        params["api_key"] = API_KEY
+
+        client["https://api.themoviedb.org/3/movie/now_playing", params, object :
+            JsonHttpResponseHandler() {
+                override fun onSuccess(statusCode: Int, headers: Headers, json: JSON) {
+                    // Access a JSON array response with `json.jsonArray`
+                    val resultsJSON : JSONArray = json.jsonObject.get("results") as JSONArray
+                    val moviesRawJSON : String = resultsJSON.toString()
+                    val gson = Gson()
+                    val arrayMovieType = object : TypeToken<List<Movie>>() {}.type
+
+                    val models : List<Movie> = gson.fromJson(moviesRawJSON, arrayMovieType)
+                    recyclerView.adapter = MovieRecyclerViewAdapter(models)
+
+                    Log.d("MovieFragment", "response successful")
+                }
+
+                override fun onFailure(
+                    statusCode: Int,
+                    headers: Headers?,
+                    response: String,
+                    throwable: Throwable?
+                ) {
+                    // If the error is not null, log it!
+                    throwable?.message?.let {
+                        Log.e("MovieFragment Error", response)
+                    }
+                }
+            }]
 
     }
 }
--- app/src/main/java/com/example/flixster/MovieRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/flixster/MovieRecyclerViewAdapter.kt
@@ -1,11 +1,60 @@
 package com.example.flixster
 
+import android.content.Intent
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
 /**
- * [RecyclerView.Adapter] that can display a [Movie] and makes a call to the
- * specified [OnListFragmentInteractionListener].
+ * [RecyclerView.Adapter] that can display a [Movie]
  */
-class MovieRecyclerViewAdapter {
+class MovieRecyclerViewAdapter(private val movieList: List<Movie>) :
+    RecyclerView.Adapter<MovieRecyclerViewAdapter.MovieViewHolder>() {
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.fragment_movie, parent, false)
+        return MovieViewHolder(view)
+    }
+
+    /**
+     * This inner class lets us refer to all the different View elements
+     * (Yes, the same ones as in the XML layout files!)
+     */
+    inner class MovieViewHolder(val mView: View) : RecyclerView.ViewHolder(mView) {
+        var mItem: Movie? = null
+        val mMovieTitle: TextView = mView.findViewById<View>(R.id.movie_title) as TextView
+        val mMovieImage: ImageView = mView.findViewById(R.id.movie_img) as ImageView
+        val mMovieDescr: TextView = mView.findViewById<View>(R.id.movie_descr) as TextView
+    }
+
+    /**
+     * This lets us "bind" each Views in the ViewHolder to its' actual data!
+     */
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        val movie = movieList[position]
+
+        holder.mItem = movie
+        holder.mMovieTitle.text = movie.title
+        holder.mMovieDescr.text = movie.overview
+
+        Glide.with(holder.mView)
+            .load("https://image.tmdb.org/t/p/w500" + movie.poster_path)
+            .centerInside()
+            .into(holder.mMovieImage)
+    }
+
+    /**
+     * Remember: RecyclerView adapters require a getItemCount() method.
+     */
+    override fun getItemCount(): Int {
+        return movieList.size
+    }
 }
